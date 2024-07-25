19c6da269869c993491a64fddee5d5189f3d0ccf
Laptop-Price-Predictor <singhkamal9901@gmail.com>
2024-07-26 02:37:20 +0530

    update

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ st.markdown("""
 col1, col2 = st.columns(2)
 
 with col1:
-    company = st.selectbox('Brand', ["None"] + list(df['Company'].unique()))
-    Type = st.selectbox('Type', ["None"] + list(df['TypeName'].unique()))
+    company = st.selectbox('Brand', ["None"]+ list(df['Company'].unique()))
+    Type = st.selectbox('Type', ["None"] + list(df['TypeName'].unique()),index=0)
     ram = st.selectbox('RAM (in GB)', ["None"] + [2, 4, 6, 8, 12, 16, 24, 32, 64])
     weight = st.number_input('Weight of the Laptop (in Kgs)', min_value=0.0, step=0.1)
     touchscreen = st.selectbox('Touchscreen', [ 'No', 'Yes'])
@@ -37,8 +37,8 @@ with col2:
                               ['None', '1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800',
                                '2560x1600', '2560x1440', '2304x1440'])
     cpu = st.selectbox('CPU', ["None"] + list(df['CpuName'].unique()))
-    hdd = st.selectbox('HDD (in GB)', ["None"] + [0, 128, 256, 512, 1024, 2048])
-    ssd = st.selectbox('SSD (in GB)', ["None"] + [0, 8, 128, 256, 512, 1024])
+    hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048])
+    ssd = st.selectbox('SSD (in GB)',  [0, 8, 128, 256, 512, 1024])
     gpu = st.selectbox('GPU', ["None"] + list(df['GpuBrand'].unique()))
     os = st.selectbox('OS', ["None"] + list(df['Os'].unique()))
 
